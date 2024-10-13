d667a7d2d03a026101b774e14d64983a476d7453
praveen.kushwaha <praveenkushwaha.tech@gmail.com>
2024-10-13 15:54:30 +0530

    categoryDto implemented

--- src/main/java/com/praveen/service/CategoryService.java
+++ src/main/java/com/praveen/service/CategoryService.java
@@ -2,11 +2,15 @@ package com.praveen.service;
 
 import java.util.List;
 
+import com.praveen.dto.CategoryDto;
+import com.praveen.dto.CategoryResponse;
 import com.praveen.entity.Category;
 
 public interface CategoryService {
 
-    public Boolean saveCategory(Category category);
+    public Boolean saveCategory(CategoryDto categoryDto);
 	
-	public List<Category> getAllCategory();
+	public List<CategoryDto> getAllCategory();
+	
+	public List<CategoryResponse> getActiveCategory();
 }
