ec3d192fec72646cfee4342a69030372296840f5
praveen.kushwaha <praveenkushwaha.tech@gmail.com>
2024-10-13 16:37:11 +0530

    Update Category Implementd

--- src/main/java/com/praveen/controller/CategoryController.java
+++ src/main/java/com/praveen/controller/CategoryController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.praveen.dto.CategoryDto;
 import com.praveen.dto.CategoryResponse;
-import com.praveen.entity.Category;
 import com.praveen.service.CategoryService;
 @RestController
 @RequestMapping("/api/v1/category")
--- src/main/java/com/praveen/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/praveen/service/impl/CategoryServiceImpl.java
@@ -33,15 +33,32 @@ public class CategoryServiceImpl implements CategoryService {
 //		category.setCreatedOn(new Date());
 		
 		Category category = mapper.map(categoryDto, Category.class);
-		category.setIsDeleted(false);
-		category.setCreatedBy(1);
-		category.setCreatedOn(new Date());
+		if (ObjectUtils.isEmpty(category.getId())) {
+			category.setIsDeleted(false);
+			category.setCreatedBy(1);
+			category.setCreatedOn(new Date());
+		} else {
+			updateCategory(category);
+		}
 	
 		Category saveCategory = categoryRepo.save(category);
 		if (ObjectUtils.isEmpty(saveCategory)) {
 			return false;
 		}
 		return true;
+	}
+	
+	private void updateCategory(Category category) {
+		Optional<Category> findById = categoryRepo.findById(category.getId());
+		if (findById.isPresent()) {
+			Category existCategory = findById.get();
+			category.setCreatedBy(existCategory.getCreatedBy());
+			category.setCreatedOn(existCategory.getCreatedOn());
+			category.setIsDeleted(existCategory.getIsDeleted());
+
+			category.setUpdatedBy(1);
+			category.setUpdatedOn(new Date());
+		}
 	}
 
 	@Override
@@ -85,5 +102,4 @@ public class CategoryServiceImpl implements CategoryService {
 		}
 		return false;
 	}
-
 }
